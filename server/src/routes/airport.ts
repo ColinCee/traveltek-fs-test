79a06fbe20cd044bad69354afea374c363ca449e
Colin Cheung <ccheung@podfather.com>
2019-10-15 13:08:30 +0100

    Remove redundant middleware

--- server/src/routes/airport.ts
+++ server/src/routes/airport.ts
@@ -2,12 +2,6 @@ import express from 'express'
 import { filterByDepature, filterByDepartureAndArrival, filterByArrival } from '@src/filters/airport'
 const departure = express.Router()
 
-// middleware that is specific to this router
-departure.use(function timeLog(req, res, next) {
-  console.log('Time: ', Date.now())
-  next()
-})
-
 departure.get('/depart/:airport', async (req, res) => {
   const airport = req.params.airport
   const data = await filterByDepature(airport)
